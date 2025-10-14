cb9c15d0cbb8fc6248ee2053d9bab67f207a618c
Ruma303 <matteoh8@hotmail.it>
2025-10-14 10:39:03 +0200

    pattern matching ok

--- Lessons/pattern-matching.py
+++ Lessons/pattern-matching.py
@@ -2,6 +2,7 @@
 #% Pattern Matching
 
 ## Esempio base
+print("\nEsempi base")
 subject_value = [1, 2, 3]
 match subject_value:
     case [p, s, t]:
@@ -9,6 +10,7 @@ match subject_value:
 
 
 ## Esempio con wildcard
+print("\nEsempi con wildcard")
 subject_value = [1, 2, 3, 4, 5, 6]
 match subject_value:
     case [p, _, t]:
@@ -17,8 +19,72 @@ match subject_value:
         print("Altri valori trovati")
 
 
+## Capture pattern
+print("\nCapture pattern")
+subject_value = [1, 2, 3]
+match subject_value:
+    case [p, s]:
+        print(f"Trovati {p} e {s}")
+    case [p, s, t]:
+        print(f"Trovati {p}, {s} e {t}")
+    case _:
+        print("Nessun valore trovato")
+
+
 ## Esempio con resto "splat"
+print("\nResto splat")
 subject_value = [1, 2, 3, 4, 5, 6]
 match subject_value:
-    case [p, *splat, t]:
-        print(f"Trovato {p} e {t}. Il resto è: {splat}")   # Output: Trovato 1 e 6. Il resto è: [2, 3, 4, 5]
+    case [i, *splat, f]:
+        print(f"Primo elemento: {i}, ultimo: {f}. Altri elementi del pattern: {splat}")
+        # Primo elemento: 1, ultimo: 6. Altri elementi del pattern: [2, 3, 4, 5]
+
+
+## Literal patterns
+print("\nLiteral pattern")
+subject_value = [1, 2, 3]
+match subject_value:
+    case [1, 2, 3]:
+        print("Trovata lista 1,2,3")
+    case [1, *rest]:
+        print(f"Il resto è {rest}")
+
+
+## Or patterns
+print("\nOr pattern")
+subject_value = ["autunno", "stagione autunnale"]
+match subject_value:
+    case ["autunno", msg] | ["inverno", msg]:
+        print(f"Nella {msg} fa più freddo")
+    case ["primavera", msg] | ["estate", msg]:
+        print(f"Nella {msg} fa più caldo")
+
+
+## As patterns
+print("\nAs pattern")
+subject_value = "estate"
+match subject_value:
+    case "primavera" | "estate" as stagione:
+        print(f"in {stagione} fa più caldo")
+    case "autunno" | "inverno" as stagione:
+        print(f"in {stagione} fa più freddo")
+
+
+## Conditional Patterns (Guards)
+print("\nConditional pattern")
+subject_value = ["pari", "50"]
+match subject_value:
+    case ["pari", valore] if int(valore) % 2 == 0:
+        print(f"{valore} è un numero pari")
+    case ["dispari", valore] if int(valore) % 2 != 0:
+        print(f"{valore} è un numero dispari")
+    case _:
+        print("C'è un errore nei dati")
+
+
+## Esempio su dizionari
+print("\nEsempio di utilizzo su dizionari")
+subject_value = {"tipo": "errore", "messaggio": "file mancante"}
+match subject_value:
+    case {"tipo": "errore", "messaggio": msg}:
+        print(f"Errore: {msg}")
