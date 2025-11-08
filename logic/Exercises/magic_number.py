77f0e21008e5ee5978d92e0d9147573995e532b3
Ruma303 <matteoh8@hotmail.it>
2025-11-08 13:17:13 +0100

    magic number ok

--- logic/Exercises/magic_number.py
+++ logic/Exercises/magic_number.py
@@ -31,3 +31,27 @@ elif (number % 2 == 0) and (number % 7 == 0):
 else:
     print(f"{number} non è un magic number")
 
+
+# Unire le condizioni
+number = 15
+
+expr1 = 12 <= number <= 124 and number % 3 == 0 and number % 5 == 0
+expr2 = 2 <= number <= 50 and number % 2 == 0
+expr3 = number < 0
+expr4 = number % 2 == 0 and number % 7 == 0
+
+if expr1 or expr2 or expr3 or expr4:
+    print(f"{number} è un magic number")
+
+else:
+    print(f"{number} non è un magic number")
+
+
+# Esempio con lista
+# Se fosse stato richiesto che tutte le condizioni siano vere, avremmo usato all().
+conditions = list([expr1, expr2, expr3, expr4])
+
+if any(conditions):
+    print(f"{number} è un magic number")
+else:
+    print(f"{number} non è un magic number")
