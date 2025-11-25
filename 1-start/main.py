167eebd9006bbafc50c87a9c34ecab47bd1cc126
Ruma303 <matteoh8@hotmail.it>
2025-11-25 10:39:27 +0100

    start

---
+++ 1-start/main.py
@@ -0,0 +1,36 @@
+
+# Commenti single line
+
+"""
+print("Codice
+non
+eseguito")
+"""
+
+print("Codice eseguito")
+
+
+# Linee fisiche e logiche
+
+s = "I \
+	love \
+	programming"
+print(s)
+
+numbers = (1, 2, 3,
+4, 5, 6)
+print(numbers)
+
+s = "Ciao" ; print(s)
+
+x = 15
+
+if x < 10:
+	s = "il numero è minore di 10"
+	print(s)
+elif x == 10:
+	s = "il numero vale 10"
+	print(s)
+else:
+	s = "il numero è maggiore di 10"
+	print(s)
---
+++ 1-start/variables.py
@@ -0,0 +1,12 @@
+# Definizione variabili
+
+x = 10 # x è una variabile che contiene l'intero 10
+nome = "Alice" # nome è una variabile che contiene la stringa "Alice"
+
+# Pseudo-costanti
+PI = 3.14159
+VELOCITA_LUCE = 299_792_458  # velocità della luce in m/s
+
+# Acquisizione da input
+nome = input("Qual è il tuo nome?")
+print(f"Piacere di conoscerti, {nome}")
